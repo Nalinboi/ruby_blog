f735b711cfa4516eed27bfc0c1d986fb407b2ce2
Nalin Aswani <38874717+Nalinboi@users.noreply.github.com>
2020-11-18 13:30:40 +1300

    Made hello rails appear
    Made a header appear on the app and got rid of the template stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  # get 'welcome/index' tells Rails to map requests 
+  # to http://localhost:3000/welcome/index to the welcome controller's index action. 
+  # This was created earlier when you ran the controller generator (rails generate controller Welcome index).
+  get 'welcome/index'
+
+  # Rails provides a resources method which can be used to declare a standard REST resource. 
+  # You need to add the article resource to the config/routes.rb so the file will look as follows:
+  resources :articles
+
+  
+  # root 'welcome#index' tells Rails to map requests to the root of the application to the 
+  # welcome controller's index action and ''
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
