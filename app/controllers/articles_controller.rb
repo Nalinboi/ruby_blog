3c9a11b901dced103d219f89c323a95801a77838
Nalin Aswani <38874717+Nalinboi@users.noreply.github.com>
2020-11-18 14:50:16 +1300

    Created a form with a create method now
    with the create method there is not an immediate error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
     def new  # Defining the 'new' method
     end
+
+    def create
+        render plain: params[:article].inspect  
+    end
   end
