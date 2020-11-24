2b0e823094e4736fdc84efdc372ebe22c964923d
Nalin Aswani <38874717+Nalinboi@users.noreply.github.com>
2020-11-24 15:25:42 +1300

    better merge params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     # Need a way to find out how to do this without hard coding the first user
     
     def current_user
-        User.first  
+        User.last 
     end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,7 @@ class CommentsController < ApplicationController
   # http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy    
   def create
     @article = Article.find(params[:article_id])
-
-    # comment_params.merge!(user_id: current_user.id)
-
-
-    @comment = @article.comments.create(comment_params)
-    @comment.user_id = current_user.id
-    @comment.save
-     
-    
+    @comment = @article.comments.create(comment_params) 
     redirect_to article_path(@article)
   end
 
@@ -23,6 +15,9 @@ class CommentsController < ApplicationController
  
   private
     def comment_params
-      params.require(:comment).permit(:body)  # we want to merge the user_id into params
+      params.require(:comment)
+            .permit(:body)  # we want to merge the user_id into params
+            .merge(user_id: current_user.id)
+
     end
 end
